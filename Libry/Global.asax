886622f9d191a6ba34d268890d9e2099357ac121
Destiny Ross <destinylross@gmail.com>
2018-07-30 19:45:58 -0500

    Add project files.

---
+++ Libry/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Libry.MvcApplication" Language="C#" %>
---
+++ Libry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Libry.Startup))]
+namespace Libry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
